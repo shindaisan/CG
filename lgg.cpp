04790a5501bc52117e22ccdb33f8e03a521e6b03
shindaisan <nanzatovarsalan@yandex.ru>
2025-04-11 09:08:32 +0800

    Add files via upload

--- lgg.cpp
+++ lgg.cpp
@@ -1,50 +1,20 @@
-#include<GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-
-
-int main(void)
+﻿#define GLEW_DLL
+#define GLFW_DLL
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+if (!glfwInit()) {
+	fprintf(stderr, "ERROR:.\n");
+	return 1;
+}
 {
-    GLFWwindow* window;
+	glfwWindowhint(GLFW_CONTEXT_VERSION_MAJOR 1);
+	glfwWindowhint(GLFW_CONTEXT_VERSION_MINOR 0);
+}
+GLFWwindow
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-    glewInit();
-    if (glewInit() != GLEW_OK)
-        std::cout << "error!" << std::endl;
+window = glfwCreateWindow(512, 512"mainwindow", Null, Null);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-    if (glewInit() != GLEW_OK)
-        std::cout << "error!" << std::endl;
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f,-0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glColor3f(0.0, 1.0, 0.0);
-        glEnd();
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+if (!window) {
+	glfwterminate();
+	return -1;
 }
